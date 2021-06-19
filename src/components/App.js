49e822640af89d91225795e8b61649325ce295ab
Oleksii Patsiuk <patsyuck@ukr.net>
2021-06-20 00:18:31 +0300

    вилучено старий код форми (до рефакторингу)

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { v4 as uuid } from 'uuid';
 import './App.css';
-import ContactForm from './ContactForm';
-import { Form } from './Form';
+import { ContactForm } from './ContactForm';
 import Filter from './Filter';
 import ContactList from './ContactList';
 
@@ -14,42 +13,11 @@ export class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    /*contacts: [],*/
     filter: '',
-    name: '',
-    number: '',
   };
 
   handleChange = property => event => {
     this.setState({ [property]: event.target.value });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    if (
-      this.state.contacts.some(
-        item => item.name.toLowerCase() === this.state.name.toLowerCase(),
-      )
-    ) {
-      alert(`${this.state.name} is already in contacts!`);
-    } else {
-      /* Push НЕ ВИКОРИСТОВУВАТИ!
-      this.state.contacts.push({
-        id: uuid(),
-        name: this.state.name,
-        number: this.state.number,
-      });*/
-      this.setState({
-        contacts: this.state.contacts.concat([
-          {
-            id: uuid(),
-            name: this.state.name,
-            number: this.state.number,
-          },
-        ]),
-      });
-    }
-    this.setState({ name: '', number: '' });
   };
 
   handlerSubmit = contact => {
@@ -66,27 +34,6 @@ export class App extends Component {
     }
   };
 
-  /*handleSubmit = event => {
-    event.preventDefault();
-    if (
-      this.state.contacts.some(
-        item => item.name.toLowerCase() === event.target[0].value.toLowerCase(),
-      )
-    ) {
-      alert(`${event.target[0].value} is already in contacts!`);
-    } else {
-      this.setState(prevState => ({
-        contacts: prevState.contacts.push({
-          id: uuid(),
-          name: event.target[0].value,
-          number: event.target[1].value,
-        })
-      }));
-      console.log(this.state)
-    }
-    event.target.reset()
-  };*/
-
   handleDelete = id => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(item => item.id !== id),
@@ -97,12 +44,7 @@ export class App extends Component {
     return (
       <div>
         <h1>Phonebook</h1>
-        <ContactForm
-          name={this.state.name}
-          number={this.state.number}
-          onChange={this.handleChange}
-          onSubmit={this.handleSubmit}
-        />
+        <ContactForm handlerSubmit={this.handlerSubmit} />
         <h2>Contacts</h2>
         <Filter filter={this.state.filter} onChange={this.handleChange} />
         <ContactList
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,33 +1,52 @@
-const ContactForm = ({ name, number, onChange, onSubmit }) => {
-  return (
-    <form onSubmit={onSubmit}>
-      <label>
-        Name
-        <input value={name} onChange={onChange('name')} />
-      </label>
-      <label>
-        Number
-        <input value={number} onChange={onChange('number')} />
-      </label>
-      <button type="submit">Add contact</button>
-    </form>
-  );
+import React, { Component } from 'react';
+import { v4 as uuid } from 'uuid';
+
+const INITIAL_STATE = {
+  name: '',
+  number: '',
 };
 
-/*const ContactForm = ({onSubmit}) => {
-  return (
-    <form onSubmit={onSubmit}>
-      <label>
-        Name
-        <input name="contactName" />
-      </label>
-      <label>
-        Number
-        <input name="contactNumber" />
-      </label>
-      <button type="submit">Add contact</button>
-    </form>
-  );
-};*/
+export class ContactForm extends Component {
+  state = { ...INITIAL_STATE };
 
-export default ContactForm;
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
+  reset = () => {
+    this.setState({ ...INITIAL_STATE });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { name, number } = this.state;
+
+    const contact = {
+      id: uuid(),
+      name: name,
+      number: number,
+    };
+    this.props.handlerSubmit(contact);
+
+    this.reset();
+  };
+
+  render() {
+    console.log(this.state);
+    const { name, number } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input name="name" value={name} onChange={this.handleChange} />
+        </label>
+        <label>
+          Number
+          <input name="number" value={number} onChange={this.handleChange} />
+        </label>
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
